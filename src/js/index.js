ba83cde9468ec4ba465551b98d24b2fd60288227
Yan Yu <bx6xbChosen1@gmail.com>
2023-10-14 22:29:45 +0600

    added parralax in all sections and wrote a script for it

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,13 @@ window.scrollTo(0, 0)
 // JS variables
 const parallaxIndex = 4
 
+// Functions
+const makeParallax = (elem, index) => {
+  elem.style.top = `${Math.floor(
+    elem.getBoundingClientRect().top / index / parallaxIndex
+  )}px`
+}
+
 // Event listeners
 window.addEventListener('scroll', () => {
   root.style.setProperty(
@@ -15,7 +22,12 @@ window.addEventListener('scroll', () => {
 })
 
 parallax.forEach((item) => {
-  item.style.top = `${Math.floor(
-    item.getBoundingClientRect().top / parallaxIndex
-  )}px`
+  const halfOfItem = Math.floor(
+    parseInt(getComputedStyle(item).getPropertyValue('height'))
+  )
+  if (item.classList.contains('parralax-long')) {
+    makeParallax(item, 3)
+  } else {
+    makeParallax(item, 1.05)
+  }
 })

40017ce1da9fb9aea8bcac5e9dd4f4d49d5e1b2f
Yan Yu <bx6xbChosen1@gmail.com>
2023-10-17 22:48:23 +0600

    drake page done, added styles for rapper's pages, wrote script for switching sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,35 @@
-// Page elements
-const root = document.querySelector(':root')
+// Page elementsment.querySelector(':root')
 const parallax = document.querySelectorAll('.parallax')
-window.scrollTo(0, 0)
+const root = document.querySelector(':root')
+const switcherHeader = document.querySelector('.switcher-header')
+const rapperInfoSection = document.querySelectorAll('.rapper-info-section')
+const bioAlbumsGallery = document.querySelector('.bio-albums-gallery')
 
 // JS variables
 const parallaxIndex = 4
+const headers = ['Biography', 'Albums', 'Gallery']
 
 // Functions
 const makeParallax = (elem, index) => {
   elem.style.top = `${Math.floor(
     elem.getBoundingClientRect().top / index / parallaxIndex
   )}px`
+}
+
+const switchSlide = () => {
+  if (count === 3) {
+    count = 0
+  } else if (count === -1) {
+    count = 2
+  }
+  rapperInfoSection.forEach((val, i) => {
+    if (i === count) {
+      val.classList.remove('hide')
+      switcherHeader.innerHTML = headers[i]
+    } else {
+      val.classList.add('hide')
+    }
+  })
 }
 
 // Event listeners
@@ -21,6 +40,9 @@ window.addEventListener('scroll', () => {
   )
 })
 
+// Call functions
+window.scrollTo(0, 0)
+
 parallax.forEach((item) => {
   const halfOfItem = Math.floor(
     parseInt(getComputedStyle(item).getPropertyValue('height'))
@@ -29,5 +51,16 @@ parallax.forEach((item) => {
     makeParallax(item, 3)
   } else {
     makeParallax(item, 1.05)
+  }
+})
+
+let count = 0
+bioAlbumsGallery.addEventListener('click', (e) => {
+  if (e.target.classList.contains('left-arrow')) {
+    count--
+    switchSlide()
+  } else if (e.target.classList.contains('right-arrow')) {
+    count++
+    switchSlide()
   }
 })

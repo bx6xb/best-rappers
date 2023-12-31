22b5949ce9ae6368453f79af03aed530b4b51d1b
Yan Yu <bx6xbChosen1@gmail.com>
2023-12-31 23:34:31 +0600

    renamed js files, added 3 entries and pug-loader in webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 import "./sass/style.sass"
-import "./js/index.js"
-import "./js/main_page.js"
-import "./js/rapper_page.js"
+import "./js/general.js"
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require("path")
+const fs = require("fs")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const CopyPlugin = require("copy-webpack-plugin")
 
+const PAGES_DIR = "./src/pug"
+const PUG_FILES = fs
+  .readdirSync(PAGES_DIR)
+  .filter((fileName) => fileName.endsWith(".pug"))
+
 module.exports = (env) => {
-  const mode = env.development || "production"
+  const mode = env.development ? "development" : "production"
   const isDev = mode === "development"
 
   console.log(`Current mode - ${mode}`)
 
   return {
     mode: mode,
-    entry: path.resolve(__dirname, "./src/index.js"),
+    entry: {
+      general: path.resolve(__dirname, "./src/index.js"),
+      mainPage: path.resolve(__dirname, "./src/js/mainPage.js"),
+      rapperPage: path.resolve(__dirname, "./src/js/rapperPage.js"),
+    },
     output: {
-      filename: "index.[contenthash].js",
+      filename: "[name].[contenthash].js",
       path: path.resolve(__dirname, "./build"),
       clean: true,
     },
@@ -51,15 +61,26 @@ module.exports = (env) => {
         },
         {
           test: /\.pug$/i,
-          type: "pug-loader",
+          loader: "pug-loader",
         },
       ],
     },
     plugins: [
       new HtmlWebpackPlugin({
         template: "./src/index.html",
+        filename: "index.html",
         inject: "body",
+        chunks: ["general", "mainPage"],
       }),
+      ...PUG_FILES.map(
+        (page) =>
+          new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `html/${page.replace(/\.pug$/, ".html")}`,
+            inject: "body",
+            chunks: ["general", "rapperPage"],
+          })
+      ),
       new MiniCssExtractPlugin({
         filename: "style.css",
       }),
